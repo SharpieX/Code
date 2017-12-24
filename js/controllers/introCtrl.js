ea587a2cb86b6fbdedc7885ee60932c228cc13b1
sharpiex <ambujsinghgangwar@gmail.com>
2017-12-24 21:45:00 +0530

    Contact form add

--- js/controllers/introCtrl.js
+++ js/controllers/introCtrl.js
@@ -1,8 +1,9 @@
 angular
 .module('stack')
-.controller('introCtrl', ['questionsService', '$scope', '$auth',
-	function (questionsService, $scope, $auth) {
+.controller('introCtrl', ['questionsService', '$scope', '$auth','$http',
+	function (questionsService, $scope, $auth, $http) {
 		angular.element('#navigation').sticky({topSpacing: 0});
+
 		$scope.goTo = function ($event) {
 			var $anchor = $($event.currentTarget);
 			var nav = $($anchor.attr('href'));
@@ -19,6 +20,28 @@ angular
 			return $auth.isAuthenticated();
 		};
 
+		$scope.contactUs = function (valid) {
+			var data = {
+				from: $scope.email,
+				text: $scope.text,
+				subject: $scope.subject
+
+			};
+
+			if (valid) {
+				$http({
+					method: 'POST',
+					url: '/api/sendEmail',
+					data: data,
+				}).then(function (res) {
+					if(res.data.error){
+						$scope.showError = true;
+					} else {
+						$scope.showSuccess = true;
+					}
+				})
+			}
+		}
 
 		$('a.totop,a#btn-scroll,a.btn-scroll,.carousel-inner .item a.btn').bind('click', function (event) {
 			var $anchor = $(this);
